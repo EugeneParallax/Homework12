e954aed33d801a42be5c61fcbb0ee4c076f6de06
EugeneParallax <gardik87@gmail.com>
2023-12-06 06:11:07 +0300

    Appending homework tasks for Homework #13

--- src/Author.java
+++ src/Author.java
@@ -1,16 +1,36 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
-    public Author( String name, String surname ){
+
+    public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
 
     // Getter for Author name:
-    public String GetAuthorName(){
+    public String GetAuthorName() {
         return this.name;
     }
-    public String GetAuthorSurname(){
+
+    public String GetAuthorSurname() {
         return this.surname;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        String a2 = other.toString();
+        return this.toString().equals(a2);
+        }
+    @Override
+    public String toString(){
+        return name + " " + surname;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,38 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Author name = new Author(name);
         Author robertSheckley = new Author("Robert", "Sheckley");
         Author frankHerbert = new Author("Frank", "Herbert");
         Author rafaelSabatini = new Author("Rafael", "Sabatini");
 
-        // Book name = new Book(name, author, date);
         Book sampleBook1 = new Book("Pyramid", robertSheckley, 1975);
         Book sampleBook2 = new Book("Dune", frankHerbert, 1965);
         Book sampleBook3 = new Book("Captain Blood", rafaelSabatini, 1992);
 
-        // Using date getter and setter to change Book date and print the change.
         int oldDate = sampleBook3.GetDate();
         sampleBook3.AdminSetDate(1922);
         System.out.println("Год издания изменен с " + oldDate + " на " + sampleBook3.date);
 
-        System.out.println("Книга = " + sampleBook1.GetName());
-        System.out.println("Автор = " + sampleBook1.GetAuthor().GetAuthorName() + " " + sampleBook1.GetAuthor().GetAuthorSurname());
-        System.out.println("Издана = "+ sampleBook1.GetDate());
+        // Testing custom toString() methods of Book + Author class.
+        System.out.println(sampleBook1);
+        System.out.println(sampleBook2);
+        System.out.println(sampleBook3);
+
+        // Testing custom .equals() methods
+        Author author1 = new Author("Name","Surname");
+        Author author2 = new Author("Name","Surname");
+        Author author3 = new Author("Noname","Noname");
+        System.out.println(author1.equals(author2));
+        System.out.println(author2.equals(author1));
+        System.out.println(author1.equals(author3));
+
+        System.out.println(sampleBook1.equals(sampleBook2));
+        Book cloneBook1 = new Book("Pyramid", robertSheckley, 1975);
+        System.out.println(sampleBook1.equals(cloneBook1));
+
+        // Testing redefined .hashCode() methods
+        System.out.println(author1.hashCode());
+        System.out.println(author2.hashCode());
+        System.out.println(sampleBook1.hashCode());
     }
-
-
 }
